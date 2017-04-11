d4945ec2438e3afccd37e2a5d17e1070ca3ba575
Megan Wiley <mwiley322@gmail.com>
2017-04-11 15:56:50 -0700

    Preparing to add second case for converting words to numbers

--- converter.rb
+++ converter.rb
@@ -3,15 +3,38 @@
 puts "HI"
 class Converter
   def convert(num)
-    case num
-    when 1..4
-      ("I" * num)
-    when 4
-      "IV"
-    when 5..8
-      "V" + ("I"*(num-5))
-    when 9..10
-      ("I"*(10-num)) + "X"
+    if /^\d+$/ =~ num.to_s
+      case num
+        when 1..3
+          ("I" * num)
+        when 4
+          "IV"
+        when 5..8
+          "V" + ("I"*(num-5))
+        when 9..10
+          ("I"*(10-num)) + "X"
+        end
+    # elsif /[a-zA-Z]/ ~= num
+    #   puts 'String has letters'
+    # else
+    #   puts "String has no numbers or letters and cannot be evaluated."
     end
   end
 end
+
+
+
+# class Converter
+#   def convert_to_romans(num)
+#     case num
+#     when 1..3
+#       ("I" * num)
+#     when 4
+#       "IV"
+#     when 5..8
+#       "V" + ("I"*(num-5))
+#     when 9..10
+#       ("I"*(10-num)) + "X"
+#     end
+#   end
+# end
--- specs/converter_spec.rb
+++ specs/converter_spec.rb
@@ -1,63 +1,64 @@
 require '../converter'
 
 RSpec.describe Converter do
+  converter = Converter.new
   describe "#convert1" do
     it "returns 'I' for the #1" do
-      expect( Converter.new.convert(1) ).to eq "I"
+      expect( converter.convert(1) ).to eq "I"
     end
   end
 
   describe "#convert2" do
     it "returns 'II' for the #2" do
-      expect( Converter.new.convert(2) ).to eq "II"
+      expect( converter.convert(2) ).to eq "II"
     end
   end
 
   describe "#convert3" do
     it "returns 'III' for the #3" do
-      expect( Converter.new.convert(3) ).to eq "III"
+      expect( converter.convert(3) ).to eq "III"
     end
   end
 
   describe "#convert4" do
     it "returns 'IV' for the #4" do
-      expect( Converter.new.convert(4) ).to eq "IV"
+      expect( converter.convert(4) ).to eq "IV"
     end
   end
 
   describe "#convert5" do
     it "returns 'V' for the #5" do
-      expect( Converter.new.convert(5) ).to eq "V"
+      expect( converter.convert(5) ).to eq "V"
     end
   end
 
   describe "#convert6" do
     it "returns 'VI' for the #6" do
-      expect( Converter.new.convert(6) ).to eq "VI"
+      expect( converter.convert(6) ).to eq "VI"
     end
   end
 
   describe "#convert7" do
     it "returns 'VII' for the #7" do
-      expect( Converter.new.convert(7) ).to eq "VII"
+      expect( converter.convert(7) ).to eq "VII"
     end
   end
 
   describe "#convert8" do
     it "returns 'VIII' for the #8" do
-      expect( Converter.new.convert(8) ).to eq "VIII"
+      expect( converter.convert(8) ).to eq "VIII"
     end
   end
 
   describe "#convert9" do
     it "returns 'IX' for the #9" do
-      expect( Converter.new.convert(9) ).to eq "IX"
+      expect( converter.convert(9) ).to eq "IX"
     end
   end
 
   describe "#convert10" do
     it "returns 'X' for the #10" do
-      expect( Converter.new.convert(10) ).to eq "X"
+      expect( converter.convert(10) ).to eq "X"
     end
   end
 end
