3b480918458f80d03538a072eb508a225c56c293
Megan Wiley <mwiley322@gmail.com>
2017-04-11 15:16:09 -0700

    Tests written and tested for numerals I-X

--- converter.rb
+++ converter.rb
@@ -3,8 +3,15 @@
 puts "HI"
 class Converter
   def convert(num)
-    if (1..3) === num
-      "I"*num
+    case num
+    when 1..4
+      ("I" * num)
+    when 4
+      "IV"
+    when 5..8
+      "V" + ("I"*(num-5))
+    when 9..10
+      ("I"*(10-num)) + "X"
     end
   end
 end
--- specs/converter_spec.rb
+++ specs/converter_spec.rb
@@ -18,4 +18,46 @@ RSpec.describe Converter do
       expect( Converter.new.convert(3) ).to eq "III"
     end
   end
+
+  describe "#convert4" do
+    it "returns 'IV' for the #4" do
+      expect( Converter.new.convert(4) ).to eq "IV"
+    end
+  end
+
+  describe "#convert5" do
+    it "returns 'V' for the #5" do
+      expect( Converter.new.convert(5) ).to eq "V"
+    end
+  end
+
+  describe "#convert6" do
+    it "returns 'VI' for the #6" do
+      expect( Converter.new.convert(6) ).to eq "VI"
+    end
+  end
+
+  describe "#convert7" do
+    it "returns 'VII' for the #7" do
+      expect( Converter.new.convert(7) ).to eq "VII"
+    end
+  end
+
+  describe "#convert8" do
+    it "returns 'VIII' for the #8" do
+      expect( Converter.new.convert(8) ).to eq "VIII"
+    end
+  end
+
+  describe "#convert9" do
+    it "returns 'IX' for the #9" do
+      expect( Converter.new.convert(9) ).to eq "IX"
+    end
+  end
+
+  describe "#convert10" do
+    it "returns 'X' for the #10" do
+      expect( Converter.new.convert(10) ).to eq "X"
+    end
+  end
 end
